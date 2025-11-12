5745f4c36a6ad6273956a23c99542daa5a559772
汪洋 <wangyang2010344@foxmail.com>
2025-11-12 19:22:16 +0800

    feat: update

--- src/counter.ts
+++ src/counter.ts
@@ -4,7 +4,7 @@ export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    element.innerHTML = `web2 from dev is ${counter} ${fn(counter)}---${add(counter)}`
+    element.innerHTML = `share-lib-update ${counter} ${fn(counter)}---${add(counter)}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
